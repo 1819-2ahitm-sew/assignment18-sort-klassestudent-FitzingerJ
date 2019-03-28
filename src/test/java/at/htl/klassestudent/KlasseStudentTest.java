c8588a154e61578529db659563a036bb372d84a1
FitzingerJ <jacki.fitzinger@gmail.com>
2019-03-28 21:35:11 +0100

    First Commit

--- src/test/java/at/htl/klassestudent/KlasseStudentTest.java
+++ src/test/java/at/htl/klassestudent/KlasseStudentTest.java
@@ -1,7 +1,52 @@
 package at.htl.klassestudent;
 
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
 
 public class KlasseStudentTest {
 
+    @Test
+    public void testA() {
+        Student[] testArray = {
+                new Student("Sepp", "Leberkäse"),
+                new Student("Günther", "Specht"),
+                new Student("Simon", "Leder"),
+                new Student("Dante", "Tante"),
+                new Student("Dr", "Gert"),
+                new Student("Vorname", "Nachname"),
+                new Student("Otto", "ottO"),
+                new Student("Nudel", "Salat"),
+                new Student("Random", "Name"),
+                new Student("Soletti", "Pringles")
+        };
+        Student[] expected = testArray.clone();
+
+        Klasse klasse = new Klasse(0, testArray);
+        klasse.sortInsertion();
+
+        Arrays.sort(expected);
+
+        assertThat(klasse.getStudents(), is(expected));
+    }
+
+    @Test
+    public void testB() {
+        Student[] testArray = {
+                new Student("Bernhard", "Ehrenmüller"),
+                new Student("Bertl", "Frank"),
+                new Student("Ohrenarzt", "Leimhofer"),
+        };
+        Student[] expected = testArray.clone();
+
+        Klasse klasse = new Klasse(0, testArray);
+        klasse.sortBubble();
+
+        Arrays.sort(expected);
+
+        assertThat(klasse.getStudents(), is(expected));
+    }
 }
